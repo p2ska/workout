9f618339e094f988b2db59b8ef67d70b6a9821b9
Andres Päsoke <Andres.Pasoke@ttu.ee>
2016-11-14 12:24:58 +0200

    maintenance fix

--- include/maintenance.php
+++ include/maintenance.php
@@ -2,16 +2,15 @@
 
 $today = time();
 
-for ($day = 0; $day < 30; $day++) {
-	$d->query("select id from workout where date = ? limit 1", [ date("Y-m-d", $today - $day * 86400) ]);
+for ($day = 30; $day >= 0; $day--) {
+    $ct = date("Y-m-d", $today - $day * 86400);
+
+	$d->query("select id from workout where date = ? limit 1", [ $ct ]);
 
 	if (!$d->rows) {
 		$d->query(
 			"insert into workout (date, workout_id, rounds, reps, descr, added) values (?, ?, ?, ?, ?, ?)",
-			[ date("Y-m-d"), 13, 0, 0, "", date("Y-m-d H:i:s") ]
+			[ $ct, 13, 0, 0, "", date("Y-m-d H:i:s") ]
 		);
-	}
-	else {
-		break;
 	}
 }
