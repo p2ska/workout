f4c6bb26b25a37b6d28711bbc2fc5762c5f6bd57
Andres Päsoke <Andres.Pasoke@ttu.ee>
2016-11-15 13:06:34 +0200

    update

--- include/maintenance.php
+++ include/maintenance.php
@@ -4,16 +4,17 @@ $current_ts = mktime(12, 0, 0, date("m"), date("d"), date("Y"));
 
 $last_entry = $d->query("select date from workout order by date desc limit 1", false, "date");
 
-if ($last_entry) {
-	list($yy, $mm, $dd) = explode("-", $last_entry);
+if (!$last_entry)
+    $last_entry = date("Y-m-d", time() - 6 * 86400);
 
-	$last_ts = mktime(12, 0, 0, $mm, $dd, $yy) + 86400;
+list($yy, $mm, $dd) = explode("-", $last_entry);
 
-	if ($last_ts && $last_ts <= $current_ts) {
-		for ($a = $last_ts; $a <= $current_ts; $a += 86400)
-			$d->query(
-				"insert into workout (date, workout_id, rounds, reps, descr, added) values (?, ?, ?, ?, ?, ?)",
-				[ date("Y-m-d", $a), 13, 0, 0, "", date("Y-m-d H:i:s") ]
-			);
-	}
+$last_ts = mktime(12, 0, 0, $mm, $dd, $yy) + 86400;
+
+if ($last_ts && $last_ts <= $current_ts) {
+	for ($a = $last_ts; $a <= $current_ts; $a += 86400)
+        $d->query(
+            "insert into workout (date, workout_id, rounds, reps, descr, added) values (?, ?, ?, ?, ?, ?)",
+            [ date("Y-m-d", $a), 13, 0, 0, "", date("Y-m-d H:i:s") ]
+        );
 }
